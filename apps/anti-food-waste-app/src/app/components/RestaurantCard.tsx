e36b46dba762610a2b98af0db7bbf93aa3fc5ec4
Joshua Nathaniel M <clsjoshua@gmail.com>
2021-10-04 02:49:42 +0700

    feat: create restaurantCard component

---
+++ apps/anti-food-waste-app/src/app/components/RestaurantCard.tsx
@@ -0,0 +1,38 @@
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonItem,
+} from '@ionic/react';
+
+interface RestaurantProps {
+  id: string;
+  name: string;
+  image: string;
+  address: string;
+  rating: number;
+}
+
+interface RestaurantCardProps {
+  data: RestaurantProps;
+}
+
+const RestaurantCard = (props: RestaurantCardProps) => {
+  console.log(props)
+  return (
+    <IonItem>
+      <IonCard>
+        <img src={props.data.image} alt={props.data.id} />
+        <IonCardHeader>
+          <IonCardSubtitle>{props.data.rating}</IonCardSubtitle>
+          <IonCardTitle>{props.data.name}</IonCardTitle>
+        </IonCardHeader>
+        <IonCardContent>{props.data.address}</IonCardContent>
+      </IonCard>
+    </IonItem>
+  );
+};
+
+export default RestaurantCard;
